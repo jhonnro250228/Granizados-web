fdce6fa54638b8307d3a19b02f9f58e997653afe
jhonnro250228 <jhonnro250228@gmail.com>
2026-01-30 13:07:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("La reserva se envio correctamente");
+}); 
